65cdc3b07b514802f2b2c99a5b93ac030cff55f5
Ammar Ibrahim <ammar.ibrahim@dubizzlelabs.com>
2025-08-11 00:47:08 +0500

    domains

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://neon-world-store.vercel.app",
-  "https://neon-world-admin.vercel.app"
+  "https://lensify-admin.vercel.app",
+  "https://lensify-frontend.vercel.app",
+  "https://lensifyco.com"
 ];
 
 module.exports = allowedOrigins;
